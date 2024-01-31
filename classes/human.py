d7ef8b3314a0be9d621aa89cb091d6c58f3dbe36
brewchetta <chett.tiller@gmail.com>
2024-01-31 11:09:19 -0500

    HUMANS BEEP BOOP

--- classes/human.py
+++ classes/human.py
@@ -1,3 +1,35 @@
 class Human:
 
-    pass
+    # self is the thing itself (the actual instance)
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self._age = 0
+        print("Human waking up")
+
+    def __repr__(self):
+        return f"Human(first_name={self.first_name}, last_name={self.last_name}, age={self._age})"
+    
+    def say_full_name(self):
+        return self.first_name + " " + self.last_name
+    
+    def happy_birthday(self):
+        print("ITS MY BIRTHDAY!")
+        self._age += 1
+        print(f"I am {self._age} years old")
+
+    def pay_taxes(self):
+        if self.age > 18:
+            print("YOU JUST LOST HALF YOUR PAYCHECK MWAHAHAHAHA")
+            print("YOINK!")
+        else:
+            print("You can keep your money... FOR NOW MWHAHAHAHA")
+            print("🤌🤌🤌🤌🤌")
+
+    @property # GETTER #
+    def age(self):
+        return self._age
+    
+    @age.setter # SETTER #
+    def age(self, new_age):
+        raise TypeError("Quit lying about your age!")
--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from classes.robot import Robot
 from classes.human import Human
 
-print('Ready to make some robots!')
+print('Ready to make some humans!')
 
-bender = Robot(name="Bender", age=6000, tv_show="Futurama")
-r2d2 = Robot(name="R2D2", age=30, tv_show="Clone Wars", car="Millennium Falcon")
-terminator = Robot(name="T-1000 / Arnold", age="???", tv_show="Terminator Franchise", car="motorcycle")
+# bender = Robot(name="Bender", age=6000, tv_show="Futurama")
+# r2d2 = Robot(name="R2D2", age=30, tv_show="Clone Wars", car="Millennium Falcon")
+# terminator = Robot(name="T-1000 / Arnold", age="???", tv_show="Terminator Franchise", car="motorcycle")
+
+h1 = Human(first_name="Mohammad", last_name="Hossain")
